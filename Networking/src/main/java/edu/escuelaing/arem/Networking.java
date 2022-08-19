4de8f26aa75e7efa75e9ad9b9c05762baac34323
Diego Leon <diegoleonb10@gmail.com>
2022-08-19 13:11:29 -0500

    Se hacen los metodos printUrl y printUrlConeccion

--- Networking/src/main/java/edu/escuelaing/arem/Networking.java
+++ Networking/src/main/java/edu/escuelaing/arem/Networking.java
@@ -1,13 +1,48 @@
 package edu.escuelaing.arem;
+import java.io.*;
+import java.net.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Networking{
+    public static void main( String[] args ) throws Exception{
+       try {
+           URL myUrl = new URL("https://www.google.com/?hl=es");
+           printUrl(myUrl);
+           URLConnection urlConnection = myUrl.openConnection();
+           printUrlConnection(urlConnection);
+           
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+    }
+
+    public static void printUrl(URL myUrl){
+        System.out.println("Protocol: " + myUrl.getProtocol());
+        System.out.println("Authority: " + myUrl.getAuthority());
+        System.out.println("Host: " + myUrl.getHost());
+        System.out.println("Port: " + myUrl.getPort());
+        System.out.println("Path: " + myUrl.getPath());
+        System.out.println("Query: " + myUrl.getQuery());
+        System.out.println("File: " + myUrl.getFile());
+        System.out.println("Ref: " + myUrl.getRef());
+    }
+
+    public static void printUrlConnection(URLConnection urlConnection){
+        Map<String, List<String>> headers = urlConnection.getHeaderFields();
+        Set<Map.Entry<String, List<String>>> entrySet = headers.entrySet();
+        for(Map.Entry<String, List<String>> entry : entrySet){
+            String headerName = entry.getKey();
+            if(headerName != null){
+                System.out.println(headerName + ":");
+            }
+            List<String> headerValue = entry.getValue();
+            for(String value : headerValue){
+                System.out.println(value);
+            }
+            System.out.println(" ");
+        }
+        
     }
 }
